1ff7afa6b7427f65c4973d56637c7bd7dbaef50e
mayidream <66461682@qq.com>
2020-05-13 20:32:28 +0800

    第四去修改

--- admin/autoload.php
+++ admin/autoload.php
@@ -3,6 +3,6 @@ spl_autoload_register(function ($ClassName){
     $filename="./lib/$ClassName.class.php";
        if (file_exists($filename)) {
            require_once $filename;
-       } 113
+       }
 });
 //$mysql=new Db('admin');
