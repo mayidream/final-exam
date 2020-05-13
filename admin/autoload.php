8cea6aaa5bca9e1eb86f776035fc003fc5c0eef8
mayidream <66461682@qq.com>
2020-05-13 20:27:50 +0800

    第三次修改

--- admin/autoload.php
+++ admin/autoload.php
@@ -1,9 +1,8 @@
 <?php
 spl_autoload_register(function ($ClassName){
     $filename="./lib/$ClassName.class.php";
-       if (file_exists($filename))
-       {
+       if (file_exists($filename)) {
            require_once $filename;
-       /
+       } 123
 });
 //$mysql=new Db('admin');
